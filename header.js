7d8da9c98ab30dc04a763673e20751b751a5bfbf
Armez <zamylosz@gmail.com>
2026-07-22 23:14:20 +0200

    Header: Hide resume

--- header.js
+++ header.js
@@ -19,7 +19,6 @@
           <a href="${BASE}/index.html"                class="tab" role="tab">Projects</a>
           <a href="${BASE}/pixelart/pixelart.html"    class="tab" role="tab">Pixel Art</a>
           <a href="${BASE}/about.html"                class="tab" role="tab">About</a>
-          <a href="${BASE}/resume.html"               class="tab" role="tab">Resume</a>
         </nav>
       </div>
     </div>
