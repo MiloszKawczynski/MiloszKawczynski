a61dfdbee230b4d69f09e1fb136126c95ab440b1
Armez <zamylosz@gmail.com>
2026-07-29 12:08:22 +0200

    Footer+Style: Make site a little bit more responsive

--- footer.js
+++ footer.js
@@ -5,13 +5,13 @@
       <span class="footer__name">Miłosz Kawczyński</span>
       <div class="footer__links">
         <a href="https://github.com/MiloszKawczynski" target="_blank" rel="noopener" class="footer__link">
-          <i class="fa-brands fa-github"></i> GitHub
+          <i class="fa-brands fa-github"></i><span class="footer__link-text"> GitHub</span>
         </a>
         <a href="https://www.linkedin.com/in/miłosz-kawczyński-9a8ab134a/" target="_blank" rel="noopener" class="footer__link">
-          <i class="fa-brands fa-linkedin"></i> LinkedIn
+          <i class="fa-brands fa-linkedin"></i><span class="footer__link-text"> LinkedIn</span>
         </a>
         <a href="mailto:kawczynskimilosz@gmail.com" class="footer__link">
-          <i class="fa-solid fa-envelope"></i> kawczynskimilosz@gmail.com
+          <i class="fa-solid fa-envelope"></i><span class="footer__link-text"> kawczynskimilosz@gmail.com</span>
         </a>
       </div>
     </div>
